bd77a4bc5a99201ca4af5d371bab8c603c24fbf7
Waldir Said <tprog.said.pereyra@outlook.com>
2021-03-10 02:01:51 -0600

    Primera parte "Recuperación de Contraseña"

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +15,56 @@ namespace ControlHrsConsultoria.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult StartRecovery()
+        {
+            Models.ViewModel.RecoveryViewModel model = new Models.ViewModel.RecoveryViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult StartRecovery(Models.ViewModel.RecoveryViewModel model)
+        {
+            try { 
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            string token = GetSha256(Guid.NewGuid().ToString());
+
+            using (Models.ControlHrsConsultoriaEntities db = new Models.ControlHrsConsultoriaEntities())
+            {
+                var oUser = db.Usuario.Where(d => d.correo == model.correo).FirstOrDefault();
+                if(oUser != null)
+                {
+                        oUser.token_recovery = token;
+                        db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                }
+            }
+
+            return View();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult Recovery()
+        {
+            return View();
+        }
+        #region HELPERS
+        private string GetSha256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+        #endregion
     }
 }
